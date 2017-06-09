0fc50d2e3fd31eac60a6034d7704f9dbc0c50a4a
Christoph Pageler <christoph.pageler@me.com>
2017-06-09 23:50:36 +0200

    added inline documentation

--- Sources/Consul+Agent.swift
+++ Sources/Consul+Agent.swift
@@ -40,7 +40,6 @@ extension Consul {
                               completion: completion)
     }
     
-    
     // MARK: - Configuration
     
     
@@ -93,7 +92,6 @@ extension Consul {
                                 completion: completion)
     }
     
-    
     // MARK: - Maintenance
     
     
@@ -139,7 +137,6 @@ extension Consul {
                          encoding: URLEncoding.queryString,
                          completion: completion)
     }
-    
     
     // MARK: - Join
     
@@ -197,6 +194,9 @@ extension Consul {
     /// Consul will attempt to reconnect, and the services and checks belonging to that node will not be cleaned up.
     /// Forcing a node into the left state allows its old entries to be removed.
     ///
+    /// - [API Documentation Graceful][apidocGraceful]
+    /// - [API Documentation Force][apidocForce]
+    ///
     /// - Parameter force: Specifices to leave gracefully or forced
     ///
     /// - Returns: Void Result
@@ -219,6 +219,10 @@ extension Consul {
                          completion: completion)
     }
     
+    /// just a small helper method which returns the correct path for agent-leave (forced)
+    ///
+    /// - Parameter force: Specifies whether the path for force of graceful shutdown should be returned
+    /// - Returns: leave path
     private func agentLeavePath(force: Bool) -> String {
         if force {
             return "/v1/agent/force-leave"
@@ -229,12 +233,30 @@ extension Consul {
     
     // MARK: - Checks
     
+    /// This endpoint returns all checks that are registered with the local agent.
+    /// These checks were either provided through configuration files or added dynamically using the HTTP API.
+    ///
+    /// It is important to note that the checks known by the agent may be different from those reported by
+    /// the catalog. This is usually due to changes being made while there is no leader elected.
+    /// The agent performs active anti-entropy, so in most situations everything will be in sync
+    /// within a few seconds.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Returns: Result with Checks
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#list-checks
+    ///
     public func agentChecks() -> QuackResult<[ConsulAgentCheckOutput]> {
         return respondWithArray(path: "/v1/agent/checks",
                                 parser: QuackArrayParserByIgnoringDictionaryKeys(),
                                 model: ConsulAgentCheckOutput.self)
     }
     
+    /// Async version of `Consul.agentChecks()`
+    ///
+    /// - SeeAlso: `Consul.agentChecks()`
+    /// - Parameter completion: completion block
     public func agentChecks(completion: @escaping (QuackResult<[ConsulAgentCheckOutput]>) -> (Void)) {
         respondWithArrayAsync(path: "/v1/agent/checks",
                               parser: QuackArrayParserByIgnoringDictionaryKeys(),
@@ -242,6 +264,15 @@ extension Consul {
                               completion: completion)
     }
     
+    /// This endpoint adds a new check to the local agent. Checks may be of script, HTTP, TCP, or TTL type.
+    /// The agent is responsible for managing the status of the check and keeping the Catalog in sync.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Parameter check: check to register
+    /// - Returns: Void Result
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#register-check
     public func agentRegisterCheck(_ check: ConsulAgentCheckInput) -> QuackVoid {
         let params = agentRegisterCheckParams(check)
         return respondVoid(method: .put,
@@ -250,7 +281,12 @@ extension Consul {
                            encoding: JSONEncoding.default)
     }
     
-    public func agentRegisterCheck(_ check: ConsulAgentCheckInput, completion: @escaping (QuackVoid) -> (Void)) {
+    /// Async version of `Consul.agentRegisterCheck(_ check: ConsulAgentCheckInput)`
+    ///
+    /// - SeeAlso: `Consul.agentRegisterCheck(_ check: ConsulAgentCheckInput)`
+    /// - Parameter completion: completion block
+    public func agentRegisterCheck(_ check: ConsulAgentCheckInput,
+                                   completion: @escaping (QuackVoid) -> (Void)) {
         let params = agentRegisterCheckParams(check)
         respondVoidAsync(method: .put,
                          path: "/v1/agent/check/register",
@@ -259,6 +295,10 @@ extension Consul {
                          completion: completion)
     }
     
+    /// helper method which returns parameters for register check
+    ///
+    /// - Parameter check: check input
+    /// - Returns: parameters
     private func agentRegisterCheckParams(_ check: ConsulAgentCheckInput) -> [String: Any] {
         var params: [String: Any] = [:]
         
@@ -287,10 +327,24 @@ extension Consul {
         return params
     }
     
+    /// This endpoint remove a check from the local agent.
+    /// The agent will take care of deregistering the check from the catalog.
+    /// If the check with the provided ID does not exist, no action is taken.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Parameter id: check id
+    /// - Returns: Void Result
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#deregister-check
     public func agentDeregisterCheck(id: String) -> QuackVoid {
         return respondVoid(method: .put, path: "/v1/agent/check/deregister/\(id)")
     }
     
+    /// Async version of `Consul.agentDeregisterCheck(id: String)`
+    ///
+    /// - SeeAlso: `Consul.agentDeregisterCheck(id: String)`
+    /// - Parameter completion: completion block
     public func agentDeregisterCheck(id: String,
                                      completion: @escaping (QuackVoid) -> (Void)) {
         respondVoidAsync(method: .put,
@@ -298,22 +352,50 @@ extension Consul {
                          completion: completion)
     }
     
+    /// This endpoint is used with a TTL type check to set the status of the check to passing and
+    /// to reset the TTL clock.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Parameter id: check id
+    /// - Returns: Void Result
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#ttl-check-pass
     public func agentCheckPass(id: String) -> QuackVoid {
         return respondVoid(path: "/v1/agent/check/pass/\(id)")
     }
     
+    /// Async version of `Consul.agentCheckPass(id: String)`
+    ///
+    /// - SeeAlso: `Consul.agentCheckPass(id: String)`
+    /// - Parameter completion: completion block
     public func agentCheckPass(id: String,
                                completion: @escaping (QuackVoid) -> (Void)) {
         respondVoidAsync(path: "/v1/agent/check/pass/\(id)",
                          completion: completion)
     }
     
+    /// This endpoint is used with a TTL type check to set the status of the check to warning and
+    /// to reset the TTL clock.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Parameters:
+    ///   - id: check id
+    ///   - note: note
+    /// - Returns: Void Result
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#ttl-check-warn
     public func agentCheckWarn(id: String,
                                note: String = "") -> QuackVoid {
         return respondVoid(path: "/v1/agent/check/warn/\(id)",
                            params: ["note": note])
     }
     
+    /// Async version of `Consul.agentCheckWarn(id: String, note: String)`
+    ///
+    /// - SeeAlso: `Consul.agentCheckWarn(id: String, note: String)`
+    /// - Parameter completion: completion block
     public func agentCheckWarn(id: String,
                                note: String = "",
                                completion: @escaping (QuackVoid) -> (Void)) {
@@ -322,12 +404,27 @@ extension Consul {
                          completion: completion)
     }
     
+    /// This endpoint is used with a TTL type check to set the status of the check to critical and
+    /// to reset the TTL clock.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Parameters:
+    ///   - id: check id
+    ///   - note: note
+    /// - Returns: Void Result
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#ttl-check-fail
     public func agentCheckFail(id: String,
                                note: String = "") -> QuackVoid {
         return respondVoid(path: "/v1/agent/check/fail/\(id)",
                            params: ["note": note])
     }
     
+    /// Async version of `Consul.agentCheckFail(id: String, note: String)`
+    ///
+    /// - SeeAlso: `Consul.agentCheckFail(id: String, note: String)`
+    /// - Parameter completion: completion block
     public func agentCheckFail(id: String,
                                note: String = "",
                                completion: @escaping (QuackVoid) -> (Void)) {
@@ -336,6 +433,17 @@ extension Consul {
                          completion: completion)
     }
     
+    /// This endpoint is used with a TTL type check to set the status of the check and to reset the TTL clock.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Parameters:
+    ///   - id: check id
+    ///   - status: new status
+    ///   - output: output string
+    /// - Returns: Void Result
+    ///
+    /// [apidoc]: https://www.consul.io/api/agent/check.html#ttl-check-update
     public func agentCheckUpdate(id: String,
                                  status: ConsulAgentCheckStatus,
                                  output: String = "") -> QuackVoid {
@@ -348,6 +456,10 @@ extension Consul {
                            encoding: JSONEncoding.default)
     }
     
+    /// Async version of `Consul.agentCheckUpdate(id: String, status: ConsulAgentCheckStatus, output: String)`
+    ///
+    /// - SeeAlso: `Consul.agentCheckUpdate(id: String, status: ConsulAgentCheckStatus, output: String)`
+    /// - Parameter completion: completion block
     public func agentCheckUpdate(id: String,
                                  status: ConsulAgentCheckStatus,
                                  output: String = "",

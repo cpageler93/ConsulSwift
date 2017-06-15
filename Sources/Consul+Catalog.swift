eda8cc8286903381c745fe2da06f39e83b3e8e2f
Christoph Pageler <c.pageler@reply.de>
2017-06-15 15:13:56 +0200

    added documentation

--- Sources/Consul+Catalog.swift
+++ Sources/Consul+Catalog.swift
@@ -15,11 +15,29 @@ extension Consul {
     
     // MARK: - Catalog
     
+    
+    /// This endpoint returns the list of all known datacenters.
+    /// The datacenters will be sorted in ascending order based on the estimated median round
+    /// trip time from the server to the servers in that datacenter.
+    ///
+    /// This endpoint does not require a cluster leader and will succeed even during an availability outage.
+    /// Therefore, it can be used as a simple check to see if any Consul servers are routable.
+    ///
+    /// [API Documentation][apidoc]
+    ///
+    /// - Returns: Result with Catalog Datacenters
+    ///
+    /// [apidoc]: https://www.consul.io/api/catalog.html#list-datacenters
+    ///
     public func catalogDatacenters() -> QuackResult<[ConsulCatalogDatacenter]> {
         return respondWithArray(path: "/v1/catalog/datacenters",
                                 model: ConsulCatalogDatacenter.self)
     }
     
+    /// Async version of `Consul.catalogDatacenters()`
+    ///
+    /// - SeeAlso: `Consul.catalogDatacenters()`
+    /// - Parameter completion: completion block
     public func catalogDatacenters(completion: @escaping (QuackResult<[ConsulCatalogDatacenter]>) -> (Void)) {
         return respondWithArrayAsync(path: "/v1/catalog/datacenters",
                                      model: ConsulCatalogDatacenter.self,
